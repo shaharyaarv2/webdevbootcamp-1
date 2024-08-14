2850e438e94e3b217f82ceb5ca5b5a983e786361
Shaharyaar <73778412+shaharyaarzaye@users.noreply.github.com>
2024-08-14 07:16:19 +0000

    complete crud with mongodb

--- mongooseExpress/seeds.js
+++ mongooseExpress/seeds.js
@@ -1,57 +1,60 @@
-const mongoose = require('mongoose')
-
+const mongoose = require('mongoose');
 const Product = require('./models/product');
 
 mongoose.connect('mongodb://127.0.0.1:27017/farmStand')
 .then(() => {
-    console.log("MONGO Connection Open!!!")
+    console.log("Connection Open!!!")
 })
 .catch(err =>{ 
-    console.log('oh no mongo connections errr')
+    console.log('oh no errr')
     console.log(err)
 })
 
-
 // const p = new Product({
-//     name: "Ruby GrapeFruit",
-//     price : 1.99,
+//     name: 'Ruby Grapefruit',
+//     price: 1.99,
 //     category: 'fruit'
 // })
 // p.save()
 //     .then(p => {
 //         console.log(p)
-//     }).catch(e => {
+//     })
+//     .catch(e => {
 //         console.log(e)
 //     })
 
-
 const seedProducts = [
     {
-        name : 'Fairy Eggplant',
-        price : 2,
-        category : 'vegetable'
+        name: 'Fairy Eggplant',
+        price: 1.00,
+        category: 'vegetable'
     },
     {
-        name : 'Organinc celery',
-        price : 1.3,
-        category : 'vegetable'
+        name: 'Organic Goddess Melon',
+        price: 4.99,
+        category: 'fruit'
     },
     {
-        name : 'Chocolate Whole Milk',
-        price : 2.3,
-        category : 'dairy'
+        name: 'Organic Mini Seedless Watermelon',
+        price: 3.99,
+        category: 'fruit'
     },
     {
-        name : 'Organinc mini seedless watermelon',
-        price : 2.33,
-        category : 'fruit'
-    }
+        name: 'Organic Celery',
+        price: 1.50,
+        category: 'vegetable'
+    },
+    {
+        name: 'Chocolate Whole Milk',
+        price: 2.69,
+        category: 'dairy'
+    },
 ]
 
 Product.insertMany(seedProducts)
-.then(res => {
-    console.log(res)
-})
-.catch(e => {
-    console.log(e)
-})
+    .then(res => {
+        console.log(res)
+    })
+    .catch(e => {
+        console.log(e)
+    })
